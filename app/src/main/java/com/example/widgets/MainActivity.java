3385f9ea5281d036b7800857685d71962f8bb8ac
a19gabos <a19gabos@student.his.se>
2020-04-21 11:32:50 +0200

    Button Test

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.myNewBtn);
+
+    }
 }
